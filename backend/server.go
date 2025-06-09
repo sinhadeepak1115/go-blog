729d1bd0f7916f1be0a80dbf0336700df028a6c1
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2025-06-09 18:00:16 +0530

    connecting database to the go backend

--- backend/server.go
+++ backend/server.go
@@ -1,39 +1,57 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 )
 
-func indexHandler(c *fiber.Ctx) error {
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
-func postHandler(c *fiber.Ctx) error {
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
-func putHandler(c *fiber.Ctx) error {
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
-func deleteHandler(c *fiber.Ctx) error {
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
 func main() {
+	connStr := "postgresql://postgres:pass@localhost:5432/blog?sslmode=disable"
+	// Connect to the database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// adding fiber for HTTP server
 	app := fiber.New()
 
-	app.Get("/", indexHandler)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
 
-	app.Post("/", postHandler)
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
 
-	app.Put("/", putHandler)
+	app.Put("/", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
 
-	app.Delete("/", deleteHandler)
+	app.Delete("/", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
